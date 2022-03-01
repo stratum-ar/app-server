38f958f5d402918e48d08f081dfefbb947a25587
hubertnakielski <01153087@pw.edu.pl>
2022-03-01 21:57:26 +0100

    Add and use lombok

--- src/main/java/com/stratum/reader/Manifest.java
+++ src/main/java/com/stratum/reader/Manifest.java
@@ -1,53 +1,30 @@
 package com.stratum.reader;
 
 import java.util.Map;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Manifest {
 
-  String app;
-  Map<String, String> info;
-  Map<String, String> launch;
+    String app;
+    Map<String, String> info;
+    Map<String, String> launch;
 
-  public String getAppIndexFile() {
-    String userHomeDir = System.getProperty("user.home");
-    String appDir = String.format("%s/stratum/apps/%s/", userHomeDir, app);
-    String fileName = launch.get("command").split(" ")[1];
+    public String getAppIndexFile() {
+        String fileName = launch.get("command").split(" ")[1];
+        return getWorkingDirectory() + fileName;
+    }
 
-    return appDir + fileName;
-  }
+    public String getWorkingDirectory() {
+        String userHomeDir = System.getProperty("user.home");
+        String appDir = String.format("%s/stratum/apps/%s/", userHomeDir, app);
 
-  public String getWorkingDirectory() {
-    String userHomeDir = System.getProperty("user.home");
-    String appDir = String.format("%s/stratum/apps/%s/", userHomeDir, app);
+        return appDir;
+    }
 
-    return appDir;
-  }
-
-  public String getFirstCommand() {
-    return launch.get("command").split(" ")[0];
-  }
-
-  public String getApp() {
-    return app;
-  }
-
-  public void setApp(String app) {
-    this.app = app;
-  }
-
-  public Map<String, String> getInfo() {
-    return info;
-  }
-
-  public void setInfo(Map<String, String> info) {
-    this.info = info;
-  }
-
-  public Map<String, String> getLaunch() {
-    return launch;
-  }
-
-  public void setLaunch(Map<String, String> launch) {
-    this.launch = launch;
-  }
+    public String getFirstCommand() {
+        return launch.get("command").split(" ")[0];
+    }
 }
